34052a55df1a0f213c32d71808e4a2f44a285827
Eran Peled <eranltd@gmail.com>
2022-07-22 12:47:40 +0300

    STABLE

--- src/controllers/events/events.ts
+++ src/controllers/events/events.ts
@@ -3,34 +3,34 @@ const winston = require("../../config/winston");
 const GEOLocationScheme = require("../../interfaces/GEOLocationEvent");
 const router = express.Router();
 
-/**
- *
- * Create and store new academy
- * @param {string} academy.name
- * @param {string} academy.ID
- * @param {Array<RuleCondition>} academy.projects
- * @param {string} academy.createdAt
- * @param {string} academy.updatedAt
- * Endpoint : POST http://localhost:<port>/academy
- */
+// /**
+//  *
+//  * Create and store new academy
+//  * @param {string} academy.name
+//  * @param {string} academy.ID
+//  * @param {Array<RuleCondition>} academy.projects
+//  * @param {string} academy.createdAt
+//  * @param {string} academy.updatedAt
+//  * Endpoint : POST http://localhost:<port>/academy
+//  */
 
-router.post("/", async (req, res) => {
-  const { name, ID, projects } = req.body;
+// router.post("/", async (req, res) => {
+//   const { name, ID, projects } = req.body;
 
-  const academyInstance = new Academy({
-    name,
-    ID,
-    projects,
-  });
+//   const academyInstance = new Academy({
+//     name,
+//     ID,
+//     projects,
+//   });
 
-  try {
-    //step 1 - save in db
-    const resp = await academyInstance.save();
-    res.status(201).json(resp);
-  } catch (err) {
-    res.status(400).json(err);
-    winston.error(err);
-  }
-});
+//   try {
+//     //step 1 - save in db
+//     const resp = await academyInstance.save();
+//     res.status(201).json(resp);
+//   } catch (err) {
+//     res.status(400).json(err);
+//     winston.error(err);
+//   }
+// });
 
 export default router;
--- src/services/services.ts
+++ src/services/services.ts
@@ -3,6 +3,7 @@ const InitKafkaMockProducer = require("./kafka/producer");
 
 export const loadAllServices = async (app) => {
   try {
+    console.log(InitKafkaMockProducer);
     await InitKafkaMockProducer();
   } catch (e) {
     console.log("Error  loading services: ", e);
