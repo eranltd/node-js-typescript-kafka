a5d0fe9a915ff38b20413fb2a561d9fa5c4bdc97
Eran Peled <eranltd@gmail.com>
2022-07-23 20:55:45 +0300

    .

--- src/controllers/events/events.ts
+++ src/controllers/events/events.ts
@@ -32,4 +32,14 @@ const router = express.Router();
 //   }
 // });
 
+// 1. POST /events/{topic_name}
+// Produce a message to a topic
+// The body is the message in JSON format.
+// This will place a new message in the topic named topic_name.
+
+// 2. GET /events/{topic_name}?timeout={ms}
+// Gets the next message from topic_name.
+// Will return 204 if there’s no message in the topic after the timeout has elapsed.
+// If a timeout is not specified, a default of 10 seconds should be used.
+
 export default router;
